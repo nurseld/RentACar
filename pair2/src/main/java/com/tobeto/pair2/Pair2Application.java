75a1e80f4cdb47524e962d482374a5be732c4598
nurseld <61293125+nurseld@users.noreply.github.com>
2023-12-11 15:46:59 +0300

    Initial commit

---
+++ pair2/src/main/java/com/tobeto/pair2/Pair2Application.java
@@ -0,0 +1,13 @@
+package com.tobeto.pair2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pair2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pair2Application.class, args);
+	}
+
+}
