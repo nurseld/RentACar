8d16d66be03ad55343e0c3724c2ca605c7a18edc
Adem Furkan Kavuş <adem.24077@gmail.com>
2023-12-13 16:06:34 +0300

    Crud Operations for Car Entity

---
+++ pair2/src/main/java/com/tobeto/pair2/core/utilities/mapper/ModelMapperService.java
@@ -0,0 +1,10 @@
+package com.tobeto.pair2.core.utilities.mapper;
+
+import org.modelmapper.ModelMapper;
+
+public interface ModelMapperService {
+    ModelMapper forResponse();
+    ModelMapper forRequest();
+
+
+}
