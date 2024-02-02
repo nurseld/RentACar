7d00321e796e87a1ac12845f848357ef42a6f15e
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-02 16:15:00 +0300

    Business rules added for Car

--- pair2/src/main/java/com/tobeto/pair2/services/rules/CarBusinessRules.java
+++ pair2/src/main/java/com/tobeto/pair2/services/rules/CarBusinessRules.java
@@ -4,6 +4,7 @@ import com.tobeto.pair2.core.exceptions.BusinessException;
 import com.tobeto.pair2.repositories.CarRepository;
 import com.tobeto.pair2.services.abstracts.ColorService;
 import com.tobeto.pair2.services.abstracts.ModelService;
+import com.tobeto.pair2.services.constants.BusinessMessages;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +16,22 @@ public class CarBusinessRules {
     private ModelService modelService;
     private ColorService colorService;
 
+
     public void checkIfCarPlateExists(String plate){
         if(carRepository.existsCarByPlate(plate)){
-            throw new BusinessException("Another car cannot be added with the same license plate.");
+            throw new BusinessException(BusinessMessages.Car.SAME_PLATE_EXISTS);
         }
     }
 
     public void checkIfModelIdExists(Integer id){
         if(modelService.existsByModelId(id)){
-            throw new BusinessException("The ModelId must exist in the database.");
+            throw new BusinessException(BusinessMessages.Car.MODEL_ID_MUST_EXISTS);
         }
     }
 
     public void checkIfColorIdExists(Integer id){
         if(colorService.existsByColorId(id)){
-            throw new BusinessException("The ColorId must exist in the database.");
+            throw new BusinessException(BusinessMessages.Car.COLOR_ID_MUST_EXISTS);
         }
     }
 
