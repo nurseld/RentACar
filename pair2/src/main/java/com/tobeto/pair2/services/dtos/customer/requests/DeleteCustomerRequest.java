4670fd77b8b1f461d6e62e1d4270e2113b38dcf1
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-02 16:08:12 +0300

    Requests were created for customers

---
+++ pair2/src/main/java/com/tobeto/pair2/services/dtos/customer/requests/DeleteCustomerRequest.java
@@ -0,0 +1,13 @@
+package com.tobeto.pair2.services.dtos.customer.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DeleteCustomerRequest {
+
+    private Integer id;
+}
