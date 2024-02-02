ae88b80fc6e15e78e4a7cdb16001d8d480dbfcb2
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-02 16:09:00 +0300

    Responses were created for customers

---
+++ pair2/src/main/java/com/tobeto/pair2/services/dtos/customer/responses/GetByIdCustomerResponse.java
@@ -0,0 +1,25 @@
+package com.tobeto.pair2.services.dtos.customer.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdCustomerResponse {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate birthDate;
+
+    private String nationalIdentity;
+
+    private Integer userId;
+}
