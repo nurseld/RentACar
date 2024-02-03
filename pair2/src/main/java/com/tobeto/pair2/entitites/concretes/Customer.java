b81fc43d65dd9966534644493ff3c3aca7f40c10
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-03 23:29:47 +0300

    Updated Customer entity

--- pair2/src/main/java/com/tobeto/pair2/entitites/concretes/Customer.java
+++ pair2/src/main/java/com/tobeto/pair2/entitites/concretes/Customer.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Table(name = "customers")
 @Entity
 @Getter
@@ -20,6 +22,13 @@ public class Customer extends BaseEntity {
 
     @Column(name="last_name")
     private String lastName;
+
+    @Column(name="birth_date")
+    private LocalDate birthDate;
+
+    @Column(name="national_id_no")
+    private String nationalIdentity;
+
 
     @ManyToOne()
     @JoinColumn(name="user_id")
