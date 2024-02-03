10bbe98b0951a313de22a54fac406e233b4c88ab
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-03 23:30:22 +0300

    code refactoring

--- pair2/src/main/java/com/tobeto/pair2/entitites/abstracts/BaseEntity.java
+++ pair2/src/main/java/com/tobeto/pair2/entitites/abstracts/BaseEntity.java
@@ -3,6 +3,8 @@ package com.tobeto.pair2.entitites.abstracts;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @MappedSuperclass
 @Data
 public abstract class BaseEntity {
@@ -11,19 +13,19 @@ public abstract class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-//    @Column(name="created_date")
-//    private LocalDate createdDate;
-//
-//    @Column(name="updated_date",nullable = true)
-//    private LocalDate updatedDate;
-//
-//    @PrePersist
-//    private void beforeAdd() {
-//        createdDate = LocalDate.now();
-//    }
-//
-//    @PreUpdate
-//    private void beforeUpdate() {
-//        updatedDate = LocalDate.now();
-//    }
+    @Column(name="created_date")
+    private LocalDate createdDate;
+
+    @Column(name="updated_date",nullable = true)
+    private LocalDate updatedDate;
+
+    @PrePersist
+    private void beforeAdd() {
+        createdDate = LocalDate.now();
+    }
+
+    @PreUpdate
+    private void beforeUpdate() {
+        updatedDate = LocalDate.now();
+    }
 }
