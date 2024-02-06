56d6700eda5e1a89253daf581db42ee56f7ba3d5
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-06 17:33:26 +0300

    code refactoring

--- pair2/src/main/java/com/tobeto/pair2/controllers/UsersController.java
+++ pair2/src/main/java/com/tobeto/pair2/controllers/UsersController.java
@@ -1,20 +1,13 @@
 package com.tobeto.pair2.controllers;
 
 
-import com.tobeto.pair2.core.services.JwtService;
 import com.tobeto.pair2.services.abstracts.UserService;
 import com.tobeto.pair2.services.dtos.user.requests.AddUserRequest;
-import com.tobeto.pair2.services.dtos.user.requests.CreateUserRequest;
-import com.tobeto.pair2.services.dtos.user.requests.LoginRequest;
 import com.tobeto.pair2.services.dtos.user.requests.UpdateUserRequest;
 import com.tobeto.pair2.services.dtos.user.responses.GetAllUserResponse;
 import com.tobeto.pair2.services.dtos.user.responses.GetByIdUserResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +18,6 @@ import java.util.List;
 public class UsersController {
 
     private final UserService userService;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/add")
     public void add(@RequestBody @Valid AddUserRequest request) {
@@ -52,22 +43,5 @@ public class UsersController {
         return userService.getById(id);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void register(@RequestBody CreateUserRequest request){
-        userService.register(request);
-    }
 
-    @PostMapping("login")
-    @ResponseStatus(HttpStatus.OK)
-    public String login(@RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-        if(authentication.isAuthenticated())
-        {
-            return jwtService.generateToken(loginRequest.getUsername());
-        }
-
-        throw new RuntimeException("Kullanıcı adı ya da şifre yanlış");
-    }
 }
