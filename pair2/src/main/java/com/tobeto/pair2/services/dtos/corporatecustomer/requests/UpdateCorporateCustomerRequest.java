abd1cee2e563d18b63f7939a3bac6b6b66b2d6ce
Nursel Demirkiran <demirkiran3@gmail.com>
2024-02-14 13:02:12 +0300

    added validations

--- pair2/src/main/java/com/tobeto/pair2/services/dtos/corporatecustomer/requests/UpdateCorporateCustomerRequest.java
+++ pair2/src/main/java/com/tobeto/pair2/services/dtos/corporatecustomer/requests/UpdateCorporateCustomerRequest.java
@@ -1,5 +1,9 @@
 package com.tobeto.pair2.services.dtos.corporatecustomer.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +13,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateCorporateCustomerRequest {
 
+    @NotBlank(message = "Company name cannot be blank")
+    @Size(min = 2, message = "Company name cannot be less than 2 characters")
     private String companyName;
+
+    @Pattern(regexp = "\\d{10}", message = "Tax number must be 10 digits")
     private String taxNo;
+
+    @NotBlank(message = "Contact name cannot be blank")
     private String contactName;
+
+    @Pattern(regexp = "^\\(\\d{3}\\) \\d{3} \\d{2} \\d{2}$", message = "Phone number must be in international format")
     private String phoneNumber;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
 }
--- pair2/src/main/java/com/tobeto/pair2/services/dtos/customer/requests/UpdateCustomerRequest.java
+++ pair2/src/main/java/com/tobeto/pair2/services/dtos/customer/requests/UpdateCustomerRequest.java
@@ -1,6 +1,6 @@
 package com.tobeto.pair2.services.dtos.customer.requests;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,15 +12,28 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UpdateCustomerRequest {
 
-    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 2, message = "First name must be at least 2 characters long")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 2, message = "Last name must be at least 2 characters long")
     private String lastName;
+
+    @Pattern(regexp = "\\d{11}", message = "National ID number must be 11 digits")
+    private String nationalIdNo;
 
     private LocalDate birthDate;
 
-    private String nationalIdentity;
+    @Pattern(regexp = "^\\(\\d{3}\\) \\d{3} \\d{2} \\d{2}$", message = "Phone number must be in the format (###) ### ## ##")
+    private String phoneNumber;
 
-    private Integer userId;
+    @Positive(message = "UserId should be a greater than 0.")
+    private int userId;
 }
