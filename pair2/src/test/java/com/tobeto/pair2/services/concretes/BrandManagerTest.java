93357cec510885c4e4faa85feeae6537bda5fd0e
Adem Furkan Kavuş <57873016+adem24077@users.noreply.github.com>
2024-02-16 15:12:52 +0300

    Add files via upload
    Test Added

---
+++ pair2/src/test/java/com/tobeto/pair2/services/concretes/BrandManagerTest.java
@@ -0,0 +1,146 @@
+package com.tobeto.pair2.services.concretes;
+
+
+import com.tobeto.pair2.core.exceptions.BusinessException;
+import com.tobeto.pair2.core.mapper.services.ModelMapperService;
+import com.tobeto.pair2.entitites.concretes.Brand;
+import com.tobeto.pair2.repositories.BrandRepository;
+
+import com.tobeto.pair2.services.dtos.brand.requests.AddBrandRequest;
+import com.tobeto.pair2.services.dtos.brand.requests.DeleteBrandRequest;
+import com.tobeto.pair2.services.dtos.brand.requests.UpdateBrandRequest;
+import com.tobeto.pair2.services.rules.BrandBusinessRules;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BrandManagerTest {
+    private BrandManager brandManager;
+
+    @Mock
+    private BrandRepository brandRepository;
+
+    @Mock
+    private ModelMapperService modelMapperService;
+
+    @Mock
+    private BrandBusinessRules brandBusinessRules;
+
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+
+
+    @BeforeEach
+    void setUp() {
+
+        MockitoAnnotations.openMocks(this);
+        ModelMapper mockedModelMapper = Mockito.mock(ModelMapper.class);
+        Mockito.when(modelMapperService.forRequest()).thenReturn(mockedModelMapper);
+        Mockito.when(modelMapperService.forResponse()).thenReturn(mockedModelMapper);
+        brandManager = new BrandManager(brandRepository,modelMapperService,brandBusinessRules);
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void brandNameShouldNotBeBlank() {
+        AddBrandRequest addBrandRequest = new AddBrandRequest();
+        addBrandRequest.setName("");
+        Set<ConstraintViolation<AddBrandRequest>> violations = validator.validate(addBrandRequest);
+
+        assertFalse(violations.isEmpty());
+
+    }
+
+    @Test
+    void brandNameHasAtLeastTwoLetters() {
+        AddBrandRequest addBrandRequest = new AddBrandRequest();
+        addBrandRequest.setName("ab");
+        Set<ConstraintViolation<AddBrandRequest>> violations = validator.validate(addBrandRequest);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void brandWithSameNameShouldNotExist() {
+        AddBrandRequest addBrandRequest = new AddBrandRequest();
+        addBrandRequest.setName("Volvo");
+
+        Mockito.when(brandRepository.existsBrandByName("Volvo")).thenReturn(true);
+
+        assertThrows(BusinessException.class, () ->{
+            brandManager.add(addBrandRequest);
+        });
+    }
+
+    @Test
+    void successfullyBrandCreated() {
+        AddBrandRequest addBrandRequest = new AddBrandRequest();
+        addBrandRequest.setName("Toyota");
+
+        Mockito.when(modelMapperService.forResponse().map(addBrandRequest, Brand.class)).thenReturn(new Brand());
+        brandManager.add(addBrandRequest);
+
+        assert true;
+    }
+    @Test
+    void successfullyBrandUpdated() {
+        UpdateBrandRequest updateBrandRequest = new UpdateBrandRequest();
+        updateBrandRequest.setId(2);
+        updateBrandRequest.setName("Opel");
+
+        Mockito.when(modelMapperService.forRequest().map(updateBrandRequest,Brand.class)).thenReturn(new Brand());
+        brandManager.update(updateBrandRequest);
+
+        assert true;
+    }
+
+    @Test
+    void successfullyBrandDeleted() {
+
+        DeleteBrandRequest deleteBrandRequest = new DeleteBrandRequest();
+        deleteBrandRequest.setId(2);
+
+        Mockito.when(modelMapperService.forRequest().map(deleteBrandRequest, Brand.class)).thenReturn(new Brand());
+
+        assert true;
+    }
+
+    @Test
+    void successfullyGetAllBrands() {
+        List<Brand> brands = new ArrayList<>();
+        Mockito.when(brandRepository.findAll()).thenReturn(brands);
+        brandManager.getAll();
+        assert true;
+    }
+
+    @Test
+    void successfullyGetBrandById() {
+        Brand brand = new Brand();
+        Mockito.when(brandRepository.findById(brand.getId())).thenReturn(Optional.of(new Brand()));
+        brandManager.getById(brand.getId());
+        assert true;
+    }
+
+}
---
+++ pair2/src/test/java/com/tobeto/pair2/services/concretes/ColorManagerTest.java
@@ -0,0 +1,123 @@
+package com.tobeto.pair2.services.concretes;
+
+import com.tobeto.pair2.core.mapper.services.ModelMapperService;
+import com.tobeto.pair2.entitites.concretes.Color;
+import com.tobeto.pair2.repositories.ColorRepository;
+import com.tobeto.pair2.services.dtos.color.requests.AddColorRequest;
+import com.tobeto.pair2.services.dtos.color.requests.DeleteColorRequest;
+import com.tobeto.pair2.services.dtos.color.requests.UpdateColorRequest;
+import com.tobeto.pair2.services.dtos.color.responses.GetAllColorResponse;
+import com.tobeto.pair2.services.rules.ColorBusinessRules;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorManagerTest {
+
+    private ColorManager colorManager;
+    @Mock
+    private ColorRepository colorRepository;
+    @Mock
+    private ModelMapperService modelMapperService;
+    @Mock
+    private ColorBusinessRules colorBusinessRules;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ModelMapper mockedModelMapper = Mockito.mock(ModelMapper.class);
+        Mockito.when(modelMapperService.forRequest()).thenReturn(mockedModelMapper);
+        Mockito.when(modelMapperService.forResponse()).thenReturn(mockedModelMapper);
+        colorManager = new ColorManager(colorRepository, modelMapperService,colorBusinessRules);
+    }
+
+    @AfterEach
+    void tearDown () {
+
+    }
+
+    @Test
+    void colorWithSameNameShouldNotExist () {
+        AddColorRequest addColorRequest = new AddColorRequest();
+        addColorRequest.setName("Blue");
+
+        Mockito.when(colorRepository.existsColorByName("Blue")).thenReturn(true);
+
+         assertThrows(RuntimeException.class, () -> {
+            colorManager.add(addColorRequest);
+        });
+    }
+
+    @Test
+    void colorSuccessfullyAdded() {
+        AddColorRequest addColorRequest = new AddColorRequest();
+        addColorRequest.setName("Black");
+
+        Mockito.when(modelMapperService.forRequest().map(addColorRequest, Color.class)).thenReturn(new Color());
+        colorManager.add(addColorRequest);
+
+        assert true;
+    }
+
+    @Test
+    void colorSuccessfullyDeleted() {
+
+        DeleteColorRequest deleteColorRequest = new DeleteColorRequest();
+        deleteColorRequest.setId(1);
+
+        Mockito.when(colorRepository.findById(1)).thenReturn(Optional.of(new Color()));
+        colorManager.delete(deleteColorRequest.getId());
+        assert true;
+
+    }
+
+    @Test
+    void colorSuccessfullyUpdated() {
+        UpdateColorRequest updateColorRequest = new UpdateColorRequest();
+        updateColorRequest.setId(4);
+        updateColorRequest.setName("Green");
+
+        Mockito.when(modelMapperService.forRequest().map(updateColorRequest,Color.class)).thenReturn(new Color());
+        colorManager.update(updateColorRequest);
+
+        assert true;
+
+    }
+
+    @Test
+    void getAllColorMethod() {
+        List<Color> colors = new ArrayList<>();
+        Mockito.when(colorRepository.findAll()).thenReturn(colors);
+        colorManager.getAll();
+        assert true;
+    }
+
+    @Test
+    void colorGetByIdMethod() {
+        Color color = new Color();
+        Mockito.when(colorRepository.findById(color.getId())).thenReturn(Optional.of(new Color()));
+        colorManager.getById(color.getId());
+        assert true;
+    }
+
+    @Test
+    void colorIdShouldExistsExceptionCase() {
+        Color color = new Color();
+        color.setId(2);
+        Mockito.when(colorRepository.findById(color.getId())).thenReturn(Optional.empty());
+        Mockito.when(modelMapperService.forResponse().map(color, GetAllColorResponse.class));
+        assertThrows(RuntimeException.class, () -> colorManager.getById(color.getId()));
+    }
+
+
+}
